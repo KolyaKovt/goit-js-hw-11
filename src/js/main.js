53a70003a4113b69a1eef923dbb01329582fcf19
Kovtunets Mykola <kovtunets73@gmail.com>
2023-12-28 12:01:53 +0100

    only table left

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,21 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.getElementById("form");
+const loader = document.querySelector(".loader");
 const input = form.elements.userInput;
 const imagesList = document.getElementById("images");
+
+// light create SimpleLightBox
+const lightbox = new SimpleLightbox(".images-list .card-link", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+// Save data after reload
+const hits = localStorage.getItem("hits");
+if (hits) {
+  fillImagesList(JSON.parse(hits));
+}
 
 form.addEventListener("submit", evt => {
   evt.preventDefault();
@@ -16,6 +29,9 @@ form.addEventListener("submit", evt => {
     .replace(" ", separateSign)
     .replace(",", separateSign);
 
+  imagesList.innerHTML = "";
+  loader.style.display = "block";
+
   fetch(
     `https://pixabay.com/api/?key=41489531-fbd647ce3ca134c20e68a1b0d&q=${q}&orientation=horizontal%safesearch=true&image_type=photo`
   )
@@ -24,63 +40,74 @@ form.addEventListener("submit", evt => {
       const hits = res.hits;
 
       if (hits.length === 0) {
-        iziToast.error({
-          message:
-            "Sorry, there are no images matching your search query. Please try again!",
-          maxWidth: "380px",
-          messageSize: 16,
-          position: "topRight",
-          iconUrl: "/close-message.svg",
-          theme: "dark",
-          color: "#fff",
-          backgroundColor: "#EF4040",
-          messageColor: "#fff",
-          titleColor: "#fff",
-          iconColor: "#fff",
-        });
+        showError(
+          "Sorry, there are no images matching your search query. Please try again!"
+        );
 
         return;
       }
 
-      const listItems = [];
+      localStorage.setItem("hits", JSON.stringify(hits));
 
-      hits.forEach(hit => {
-        const a = document.createElement("a");
-        a.className = "card-link";
-        a.href = `${hit.largeImageURL}`;
-        a.innerHTML = `
-        <img class="image" src="${hit.webformatURL}" alt="${hit.tags}" />
-        <table class="image-desciption-table">
-          <tr class="tr">
-            <th>Likes</th>
-            <th>Views</th>
-            <th>Comments</th>
-            <th>Downloads</th>
-          </tr>
-          <tr class="tr">
-            <td>${hit.likes}</td>
-            <td>${hit.views}</td>
-            <td>${hit.comments}</td>
-            <td>${hit.downloads}</td>
-          </tr>
-        </table>
-        `;
+      fillImagesList(hits);
 
-        listItems.push(a);
-      });
-
-      imagesList.innerHTML = "";
-      imagesList.append(...listItems);
-
-      const lightbox = new SimpleLightbox(".images-list .card-link", {
-        captionDelay: 250,
-        captionsData: "alt"
-      });
-      
       form.reset();
     })
     .catch(err => {
       console.error(err);
+      showError("Something unexpected happened! Check the console.");
+    })
+    .finally(() => {
+      loader.style.display = "none";
     });
-
 });
+
+function showError(errorText) {
+  iziToast.error({
+    message: errorText,
+    maxWidth: "380px",
+    messageSize: 16,
+    position: "topRight",
+    iconUrl: "/close-message.svg",
+    theme: "dark",
+    color: "#fff",
+    backgroundColor: "#EF4040",
+    messageColor: "#fff",
+    titleColor: "#fff",
+    iconColor: "#fff",
+  });
+}
+
+function fillImagesList(hits) {
+  const listItems = [];
+
+  hits.forEach(hit => {
+    const li = document.createElement("li");
+    li.className = "images-item";
+    li.innerHTML = `
+    <a class="card-link" href="${hit.largeImageURL}">
+      <img class="card-image" src="${hit.webformatURL}" alt="${hit.tags}" />
+    </a>
+    <table class="image-desciption-table">
+      <tr class="description-row">
+        <th class="description-column">Likes</th>
+        <th class="description-column">Views</th>
+        <th class="description-column">Comments</th>
+        <th class="description-column">Downloads</th>
+      </tr>
+      <tr class="description-row">
+        <td class="description-column">${hit.likes}</td>
+        <td class="description-column">${hit.views}</td>
+        <td class="description-column">${hit.comments}</td>
+        <td class="description-column">${hit.downloads}</td>
+      </tr>
+    </table>
+    `;
+
+    listItems.push(li);
+  });
+
+  imagesList.append(...listItems);
+
+  lightbox.refresh();
+}
